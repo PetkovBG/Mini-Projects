98a767696576cb22ce1d7bcbe0afbbbc388778ee
PetkovBG <97334281+PetkovBG@users.noreply.github.com>
2022-10-31 15:20:14 +0200

    Add files via upload

---
+++ heartAnimation/heart.js
@@ -0,0 +1,15 @@
+const bodyEl = document.querySelector('body');
+bodyEl.addEventListener('mousemove', (e)=>{
+    const xPosition = e.offsetX;
+    const yPosition = e.offsetY;
+    const spanEl = document.createElement('span');
+    spanEl.style.left = xPosition + 'px';
+    spanEl.style.top = yPosition + 'px';
+    const size = Math.random() * 100;
+    spanEl.style.width = size + 'px';
+    spanEl.style.height = size + 'px';
+    bodyEl.appendChild(spanEl);
+    setTimeout(() => {
+        spanEl.remove();
+    }, 2000);
+});
